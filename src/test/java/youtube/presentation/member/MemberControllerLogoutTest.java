1b660d0e66bff9f99688f38d15a6d291a355ff51
Jung-YouHwan <yhwjd99@gmail.com>
2023-09-06 15:51:10 +0900

    API 테스트 수정 - 로그아웃

--- src/test/java/youtube/presentation/member/MemberControllerLogoutTest.java
+++ src/test/java/youtube/presentation/member/MemberControllerLogoutTest.java
@@ -1,10 +1,16 @@
 package youtube.presentation.member;
 
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import youtube.util.ControllerTest;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static youtube.global.constant.StringConstant.ACCESS_TOKEN;
 
@@ -15,7 +21,18 @@ public class MemberControllerLogoutTest extends ControllerTest {
     void logoutFail() throws Exception {
         // expected
         mockMvc.perform(post("/api/logout"))
-                .andExpect(status().isUnauthorized());
+                .andExpect(status().isUnauthorized())
+                .andDo(document("로그아웃 실패 - 401 (로그인 상태가 아님)",
+                        preprocessResponse(prettyPrint()),
+                        resource(ResourceSnippetParameters.builder()
+                                .tag("회원")
+                                .summary("로그아웃")
+                                .responseFields(
+                                        fieldWithPath("statusCode").description("닉네임"),
+                                        fieldWithPath("message").description("오류 메세지")
+                                )
+                                .build()
+                        )));
     }
 
     @Test
@@ -29,6 +46,15 @@ public class MemberControllerLogoutTest extends ControllerTest {
         mockMvc.perform(post("/api/logout")
                         .header(ACCESS_TOKEN.value, accessToken)
                 )
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(document("로그아웃 성공",
+                        resource(ResourceSnippetParameters.builder()
+                                .tag("회원")
+                                .summary("로그아웃")
+                                .requestHeaders(
+                                        headerWithName(ACCESS_TOKEN.value).description("AccessToken")
+                                )
+                                .build()
+                        )));
     }
 }
