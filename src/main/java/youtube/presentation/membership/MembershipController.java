994ed5cac0ce6e47540659358e84465754f6872e
Jung-YouHwan <yhwjd99@gmail.com>
2023-10-31 09:18:05 +0900

    feat(controller) : 회원의 채널 멤버쉽 탈퇴

--- src/main/java/youtube/presentation/membership/MembershipController.java
+++ src/main/java/youtube/presentation/membership/MembershipController.java
@@ -1,10 +1,8 @@
 package youtube.presentation.membership;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import youtube.application.membership.MembershipCreator;
+import youtube.application.membership.MembershipDeleter;
 import youtube.domain.member.vo.MemberSession;
 import youtube.global.annotation.Login;
 
@@ -13,14 +11,23 @@ import youtube.global.annotation.Login;
 public class MembershipController {
 
     private final MembershipCreator membershipCreator;
+    private final MembershipDeleter membershipDeleter;
 
-    public MembershipController(final MembershipCreator membershipCreator) {
+    public MembershipController(final MembershipCreator membershipCreator,
+                                final MembershipDeleter membershipDeleter) {
         this.membershipCreator = membershipCreator;
+        this.membershipDeleter = membershipDeleter;
     }
 
     @PostMapping("/membership/{channelId}")
     public void joinMembership(@Login final MemberSession memberSession,
                                @PathVariable final long channelId) {
         membershipCreator.joinMembership(memberSession.id(), channelId);
+    }
+
+    @DeleteMapping("/membership/{channelId}")
+    public void withdrawMembership(@Login final MemberSession memberSession,
+                                   @PathVariable final long channelId) {
+        membershipDeleter.withdraw(memberSession.id(), channelId);
     }
 }
